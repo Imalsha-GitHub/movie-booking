1fdd48d9a6329423db3a390f547eb089408880d3
Imalsha Jathunarachchi <imalsha.contact@gmail.com>
2025-07-28 10:15:13 +0530

    movie details page and Loading component created

---
+++ OneDrive/Desktop/movie/frontend/src/components/Loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center items-center h-[80vh]'>
+        <div className="animate-spin rounded-full h-14 w-14 border-4 border-t-primary border-r-transparent border-b-transparent border-l-transparent"></div>
+    </div>
+  )
+}
+
+export default Loading
---
+++ OneDrive/Desktop/movie/readme.md
@@ -0,0 +1,3 @@
+npm create vite@latest
+lucide react for icons
+user authenticaion - clerk.com
