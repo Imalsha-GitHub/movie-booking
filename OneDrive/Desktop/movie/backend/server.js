cc817f63d9d1c305a1c969b813190b3a4e238c39
Imalsha Jathunarachchi <imalsha.contact@gmail.com>
2025-07-29 15:36:31 +0530

    backend with clerk and inngest

---
+++ OneDrive/Desktop/movie/backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import { connectDB } from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from 'inngest/express';
+import { functions, inngest } from './inngest/index.js';
+
+const app = express();
+const port = 3000;
+
+await connectDB();
+
+// Middleware
+// built-in middleware parses incoming JSON request bodies.
+app.use(express.json());
+
+// allows backend server to accept requests from different origins
+app.use(cors());
+app.use(clerkMiddleware())
+
+// API Routes
+app.get('/', (req, res) => res.send("iMovie Backend Server is running"));
+app.use('/api/inngest', serve({client: inngest, functions}))
+
+// starts Express server, making it listen for incoming HTTP requests on the specified port
+app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
--- OneDrive/Desktop/movie/readme.md
+++ OneDrive/Desktop/movie/readme.md
@@ -1,3 +1,11 @@
 npm create vite@latest
 lucide react for icons
 user authenticaion - clerk.com
+
+
+backend
+npm init -y
+npm install express cors dotenv mongoose axios cloudinary
+npm install nodemon --save-dev
+
+npm run backend
