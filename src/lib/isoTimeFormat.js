8753f72c7d101e88e1323f38c9d8a3b54d31f5fe
Imalsha Jathunarachchi <imalsha.contact@gmail.com>
2025-07-29 16:01:08 +0530

    gitignore

--- src/lib/isoTimeFormat.js
+++
@@ -1,9 +0,0 @@
-export const isoTimeFormat = (dateTime) => {
-    const date = new Date(dateTime);
-    const localTime = date.toLocaleDateString('en-US', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: true,
-    });
-    return localTime;
-}
